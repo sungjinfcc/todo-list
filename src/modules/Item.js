a0ed29784d566201f61f5d1bd2092969a0158d10
sungjin <ysoon0912@gmail.com>
2023-06-06 09:12:13 +0900

    Create skeleton html, css

---
+++ src/modules/Item.js
@@ -0,0 +1,9 @@
+export default class Item {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.isDone = false;
+  }
+}
